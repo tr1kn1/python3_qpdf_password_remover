6cb458d91897339aac3447a1befdfadca057fd1b
T K <trischni@icloud.com>
2021-06-10 16:28:20 +0200

    add blank escape in paths, add help function to print usage instructions

--- pdf_batch_decryption.py
+++ pdf_batch_decryption.py
@@ -1,9 +1,13 @@
 import os
 
+def help():
+    print("Use with the following arguments:\n1. Path containing the encrypted PDF(s)\n2. The password to open and decrypt the PDF(s)"
+    "\nExample: pdf_batch_decryption.py $PATH_TO_PDF(S) $PASSWORD")
+
 class cd:
     """Context manager class for changing the current working directory"""
     def __init__(self, new_path):
-        self.newPath = os.path.expanduser(new_path)
+        self.new_path = os.path.expanduser(new_path)
 
     def __enter__(self):
         self.saved_path = os.getcwd()
@@ -17,18 +21,23 @@ import sys
 from os import system, getcwd, listdir
 from os.path import isfile, join
 
-mypath = sys.argv[1]
-mypass = sys.argv[2]
 
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f)) and f.endswith(".pdf")]
+if sys.argv[1] == "--help" or len(sys.argv) != 3:
+    help()
+    sys.exit(0)
 
-print("Password to use for decryption: {0}".format(mypass))
+my_path = sys.argv[1]
+my_pass = sys.argv[2]
+
+# Get all files with the extension ".pdf"
+only_files = [f for f in listdir(my_path) if isfile(join(my_path, f)) and f.endswith(".pdf")]
+
+print("Password to use for decryption: {0}".format(my_pass))
 
 with cd(mypath):
     print("PDF containing folder:\n{0}\n".format(getcwd()))
-    for f in onlyfiles:
+    for f in only_files:
         infile = f
         outfile = f[:-4] + "_OPEN.pdf"
-        print("Decrypt: {0} to {1}".format(infile, outfile))
-        system("qpdf --password={0} --decrypt {1} {2}".format(mypass, infile, outfile))
-
+        print("Decrypt: '{0}' to '{1}'".format(infile, outfile))
+        system("qpdf --password={0} --decrypt '{1}' '{2}'".format(my_pass, infile, outfile))
